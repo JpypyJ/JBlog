94b90c3d09f2e63f7371b339f415f56b275703f9
Jpy <Jpy@qq.com>
2023-01-09 21:04:52 +0800

    style(JBlog): update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -125,7 +125,7 @@ module.exports = {
     // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
 
     bodyBgImg: [
-      'JBlog/img/mybg.jpg'
+      'https://jpypyj-img-1310948243.cos.ap-guangzhou.myqcloud.com/202301092055706.jpg'
     ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时每隔15秒换一张。
     bodyBgImgOpacity: 0.8, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
 
