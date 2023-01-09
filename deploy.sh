160231e2d8958ce48576e18e23c4771b579d90ba
Jpy <Jpy@qq.com>
2023-01-09 12:46:52 +0800

    style(JBlog): update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@
 set -e
 
 
-push_addr=`git@github.com:JpypyJ/JBlog.git` # git提交地址，也可以手动设置，比如：push_addr=git@github.com:xugaoyi/vuepress-theme-vdoing.git
 commit_info=`git describe --all --always --long`
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
-push_branch=gh-pages # 推送的分支
 
 # 生成静态文件
 npm run build
@@ -18,7 +16,7 @@ cd $dist_path
 git init
 git add -A
 git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+git push -f git@github.com:JpypyJ/JBlog.git master:gh-pages
 
 cd -
 rm -rf $dist_path
